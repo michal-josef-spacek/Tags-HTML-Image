b5f711eb46985d868b438234e584d117aea86a3e
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-04-20 15:22:36 +0200

    Add doc and examples
    Examples are generated from doc.

--- Image.pm
+++ Image.pm
@@ -277,3 +277,370 @@ sub _process_css {
 
 __END__
 
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Tags::HTML::Image - Tags helper class for image presentation.
+
+=head1 SYNOPSIS
+
+ use Tags::HTML::Image;
+
+ my $obj = Tags::HTML::Image->new(%params);
+ $obj->cleanup;
+ $obj->init($image);
+ $obj->prepare;
+ $obj->process;
+ $obj->process_css;
+
+=head1 METHODS
+
+=head2 C<new>
+
+ my $obj = Tags::HTML::Image->new(%params);
+
+Constructor.
+
+=over 8
+
+=item * C<css_class>
+
+Image CSS class.
+
+Default value is 'image'.
+
+=item * C<css_comment_height>
+
+Image comment height (in pixels).
+
+Default value is 50.
+
+=item * C<fit_minus>
+
+Length to minus of image fit.
+
+Default value is undef.
+
+=item * C<img_comment_cb>
+
+Image comment callback.
+
+Default value is undef.
+
+=item * C<img_select_cb>
+
+Image select callback.
+
+Default value is undef.
+
+=item * C<img_src_cb>
+
+Image src callback across data object.
+
+Default value is undef.
+
+=item * C<img_width>
+
+Image width in pixels.
+
+Default value is undef.
+
+=item * C<tags>
+
+'L<Tags::Output>' object.
+
+Default value is undef.
+
+=item * C<title>
+
+Image title.
+
+Default value is undef.
+
+=back
+
+Returns instance of object.
+
+=head2 C<cleanup>
+
+ $obj->cleanup;
+
+Process cleanup after page run.
+
+Returns undef.
+
+=head2 C<init>
+
+ $obj->init($image);
+
+Process initialization in page run.
+
+Take L<Data::Image> object as C<$image>,
+
+Returns undef.
+
+=head2 C<prepare>
+
+ $obj->prepare;
+
+Process initialization before page run.
+
+It is not used in this module.
+
+Returns undef.
+
+=head2 C<process>
+
+ $obj->process;
+
+Process L<Tags> structure for output with hello world message.
+
+Returns undef.
+
+=head2 C<process_css>
+
+ $obj->process_css;
+
+Process L<CSS::Struct> structure.
+
+Returns undef.
+
+=head1 ERRORS
+
+ new():
+         From Class::Utils::set_params():
+                 Unknown parameter '%s'.
+         From Tags::HTML::new():
+                 Parameter 'css' must be a 'CSS::Struct::Output::*' class.
+                 Parameter 'tags' must be a 'Tags::Output::*' class.
+         Parameter 'img_comment_cb' must be a code.
+         Parameter 'img_select_cb' must be a code.
+         Parameter 'img_src_cb' must be a code.
+
+ init():
+         Image object is required.
+         Image object must be a instance of 'Data::Image'.
+         No image URL.
+
+ process():
+         From Tags::HTML::process():
+                 Parameter 'tags' isn't defined.
+
+ process_css():
+         From Tags::HTML::process_css():
+                 Parameter 'css' isn't defined.
+
+=head1 EXAMPLE1
+
+=for comment filename=create_image_and_print_html.pl
+
+ use strict;
+ use warnings;
+
+ use CSS::Struct::Output::Indent;
+ use Data::Image;
+ use DateTime;
+ use Tags::HTML::Image;
+ use Tags::Output::Indent;
+
+ # Object.
+ my $css = CSS::Struct::Output::Indent->new;
+ my $tags = Tags::Output::Indent->new;
+ my $obj = Tags::HTML::Image->new(
+         'css' => $css,
+         'tags' => $tags,
+ );
+
+ # Definition of image.
+ my $image = Data::Image->new(
+         'author' => 'Zuzana Zonova',
+         'comment' => 'Michal from Czechia',
+         'dt_created' => DateTime->new(
+                 'day' => 1,
+                 'month' => 1,
+                 'year' => 2022,
+         ),
+         'height' => 2730,
+         'size' => 1040304,
+         'url' => 'https://upload.wikimedia.org/wikipedia/commons/a/a4/Michal_from_Czechia.jpg',
+         'width' => 4096,
+ );
+
+ # Init.
+ $obj->init($image);
+
+ # Process HTML and CSS.
+ $obj->process;
+ $obj->process_css;
+
+ # Print out.
+ print "HTML:\n";
+ print $tags->flush;
+ print "\n\n";
+ print "CSS:\n";
+ print $css->flush;
+
+ # Output:
+ # HTML:
+ # <figure class="image">
+ #   <img alt="Michal from Czechia" src=
+ #     "https://upload.wikimedia.org/wikipedia/commons/a/a4/Michal_from_Czechia.jpg"
+ #     >
+ #   </img>
+ #   <figcaption>
+ #     Michal from Czechia
+ #   </figcaption>
+ # </figure>
+ # 
+ # CSS:
+ # .image img {
+ #         display: block;
+ #         height: 100%;
+ #         width: 100%;
+ #         object-fit: contain;
+ # }
+ # .image {
+ #         height: calc(100vh);
+ # }
+ # .image figcaption {
+ #         position: absolute;
+ #         bottom: 0;
+ #         background: rgb(0, 0, 0);
+ #         background: rgba(0, 0, 0, 0.5);
+ #         color: #f1f1f1;
+ #         width: 100%;
+ #         transition: .5s ease;
+ #         opacity: 0;
+ #         font-size: 25px;
+ #         padding: 12.5px 5px;
+ #         text-align: center;
+ # }
+ # figure.image:hover figcaption {
+ #         opacity: 1;
+ # }
+
+=head1 EXAMPLE2
+
+=for comment filename=plack_app_image.pl
+
+ use strict;
+ use warnings;
+ 
+ use CSS::Struct::Output::Indent;
+ use Data::Image;
+ use DateTime;
+ use Plack::App::Tags::HTML;
+ use Plack::Runner;
+ use Tags::Output::Indent;
+ 
+ my $image = Data::Image->new(
+ 	'author' => 'Zuzana Zonova',
+ 	'comment' => 'Michal from Czechia',
+ 	'dt_created' => DateTime->new(
+ 		'day' => 1,
+ 		'month' => 1,
+ 		'year' => 2022,
+ 	),
+ 	'height' => 2730,
+ 	'size' => 1040304,
+ 	'url' => 'https://upload.wikimedia.org/wikipedia/commons/a/a4/Michal_from_Czechia.jpg',
+ 	'width' => 4096,
+ );
+ 
+ my $app = Plack::App::Tags::HTML->new(
+ 	'component' => 'Tags::HTML::Image',
+ 	'css' => CSS::Struct::Output::Indent->new,
+ 	'data_init' => [$image],
+ 	'tags' => Tags::Output::Indent->new(
+ 		'xml' => 1,
+ 		'preserved' => ['style'],
+ 	),
+ 	'title' => 'Image',
+ )->to_app;
+ Plack::Runner->new->run($app);
+
+ # Output (GET /):
+ # <!DOCTYPE html>
+ # <html lang="en">
+ #   <head>
+ #     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ #     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+ #     <title>
+ #       Image
+ #     </title>
+ #     <style type="text/css">
+ # * {
+ #         box-sizing: border-box;
+ #         margin: 0;
+ #         padding: 0;
+ # }
+ # .image img {
+ #         display: block;
+ #         height: 100%;
+ #         width: 100%;
+ #         object-fit: contain;
+ # }
+ # .image {
+ #         height: calc(100vh);
+ # }
+ # .image figcaption {
+ #         position: absolute;
+ #         bottom: 0;
+ #         background: rgb(0, 0, 0);
+ #         background: rgba(0, 0, 0, 0.5);
+ #         color: #f1f1f1;
+ #         width: 100%;
+ #         transition: .5s ease;
+ #         opacity: 0;
+ #         font-size: 25px;
+ #         padding: 12.5px 5px;
+ #         text-align: center;
+ # }
+ # figure.image:hover figcaption {
+ #         opacity: 1;
+ # }
+ # </style>
+ #   </head>
+ #   <body>
+ #     <figure class="image">
+ #       <img alt="Michal from Czechia" src=
+ #         "https://upload.wikimedia.org/wikipedia/commons/a/a4/Michal_from_Czechia.jpg"
+ #         />
+ #       <figcaption>
+ #         Michal from Czechia
+ #       </figcaption>
+ #     </figure>
+ #   </body>
+ # </html>
+
+=head1 DEPENDENCIES
+
+L<Class::Utils>,
+L<Error::Pure>,
+L<Scalar::Util>,
+L<Tags::HTML>.
+
+=head1 REPOSITORY
+
+L<https://github.com/michal-josef-spacek/Tags-HTML-Image>
+
+=head1 AUTHOR
+
+Michal Josef Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+© 2022-2024 Michal Josef Špaček
+
+BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
